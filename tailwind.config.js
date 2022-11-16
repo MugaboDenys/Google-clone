d174524a0f61482187662358c1e12cd69a885b25
Mugabodenys <mugabodan2020@gmail.com>
2022-11-16 08:27:05 +0200

    Made requested changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,22 @@ module.exports = {
         'btn' : '0 0 2px gray',
         'btn2' : '0 3px 5px -5px gray',
         'input' : '0 0 6px gray'
+      },
+      colors : {
+        'pale-blue' : '#1a73e8',
+        'dark-blue' : '#115ec3'
+      },
+      fontSize : {
+        'smaller' : '.7rem'
+      },
+      width: {
+        'xs' : '13rem',
+        'sm' : '18rem'
+      },
+      minWidth : {
+        'xsm' : '10rem',
+        'sm' : '20rem',
+        'base' : '24rem'
       }
 
     },
