958555d25f7e9f75b227151f2f37e13e8c0d647b
Mugabodenys <mugabodan2020@gmail.com>
2022-11-21 13:50:23 +0200

    Change namings in Tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     extend: {
-      fontFamily : {
-        roboto : ['Roboto', 'sans-serif']
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
       },
-      boxShadow : {
-        btn : '0 0 2px gray',
-        btn2 : '0 3px 5px -5px gray',
-        input : '0 0 6px gray'
+      boxShadow: {
+        btn: "0 0 2px gray",
+        btn2: "0 3px 5px -5px gray",
+        input: "0 0 6px gray",
       },
-      colors : {
-        paleblue : '#1a73e8',
-        darkblue : '#115ec3'
+      colors: {
+        paleblue: "#1a73e8",
+        darkblue: "#115ec3",
       },
-      fontSize : {
-        smaller : '.7rem'
+      fontSize: {
+        smaller: ".7rem",
       },
       width: {
-        xs : '13rem',
-        sm : '18rem'
+        xs: "13rem",
+        sm: "18rem",
       },
-      minWidth : {
-        xsm : '10rem',
-        sm : '20rem',
-        base : '24rem'
+      minWidth: {
+        xsm: "10rem",
+        sm: "20rem",
+        base: "24rem",
       },
-      borderRadius : {
-        btn : '.25rem'
-      }
-
-
+      borderRadius: {
+        btn: ".25rem",
+      },
     },
   },
   plugins: [],
-}
+};
