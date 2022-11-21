f5afb15d0e023b9380d7ea3c6f77b368ef4da9e5
Mugabodenys <mugabodan2020@gmail.com>
2022-11-21 13:49:13 +0200

    Change namings in Tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,31 +4,31 @@ module.exports = {
   theme: {
     extend: {
       fontFamily : {
-        'roboto' : ['Roboto', 'sans-serif']
+        roboto : ['Roboto', 'sans-serif']
       },
       boxShadow : {
-        'btn' : '0 0 2px gray',
-        'btn2' : '0 3px 5px -5px gray',
-        'input' : '0 0 6px gray'
+        btn : '0 0 2px gray',
+        btn2 : '0 3px 5px -5px gray',
+        input : '0 0 6px gray'
       },
       colors : {
-        'pale-blue' : '#1a73e8',
-        'dark-blue' : '#115ec3'
+        paleblue : '#1a73e8',
+        darkblue : '#115ec3'
       },
       fontSize : {
-        'smaller' : '.7rem'
+        smaller : '.7rem'
       },
       width: {
-        'xs' : '13rem',
-        'sm' : '18rem'
+        xs : '13rem',
+        sm : '18rem'
       },
       minWidth : {
-        'xsm' : '10rem',
-        'sm' : '20rem',
-        'base' : '24rem'
+        xsm : '10rem',
+        sm : '20rem',
+        base : '24rem'
       },
       borderRadius : {
-        'btn' : '.25rem'
+        btn : '.25rem'
       }
 
 
